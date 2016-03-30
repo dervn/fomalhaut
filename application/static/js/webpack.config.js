5fde6e66421cd09b8fcc055eaed0d42622a2afcf
baikui liu <liubaikui@ucfgroup.com>
2016-03-30 12:24:05 +0800

    修改webpack js文件分开生成

--- application/static/js/webpack.config.js
+++ application/static/js/webpack.config.js
@@ -1,11 +1,15 @@
+//var webpack = require('webpack');
+//var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+
 module.exports = {
     entry : {
-        index : "./index.js"
+        helloworld : "./helloworld.js"
     },
     output : {
         path : "./build",
-        filename : "bundle.js"
+        filename: '[name].js' // Template based on keys in entry above
     },
+    //plugins: [commonsPlugin],
     module : {
         loaders : [
             {test:/\.js$/, loader:'jsx-loader'}
