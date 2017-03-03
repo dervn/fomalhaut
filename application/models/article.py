60b501a04ceeb69061c59c317d38e26fe6657a0f
baikui liu <waituy@gmail.com>
2017-03-03 23:01:06 +0800

    更新MODEL

--- application/models/article.py
+++ application/models/article.py
@@ -1,10 +1,9 @@
 # coding: utf-8
-import datetime
 from ._base import db
 
-class User(db.Model):
+class Article(db.Model):
 
-    __tablename__ = 'tb_users'
+    __tablename__ = 'tb_article'
 
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), default='')
